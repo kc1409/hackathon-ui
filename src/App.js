adf9cc967a779540423307664a95618a4bb7cc57
amet-vikram13 <amet97vikram@gmail.com>
2022-09-11 21:05:12 +0530

    added and setup component library react-native-elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaProvider>
+      <View>
+        <Text>App</Text>
+      </View>
+    </SafeAreaProvider>
   )
 }
 
-export default App
+export default App;
